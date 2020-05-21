3df17dfb722882b1f06d29865378d3d86b14e750
natagomezgarz <natagomezgarz@unal.edu.co>
2020-05-21 13:07:58 -0500

    Added the possibility of entering characters

--- Proyecto/README.md
+++ Proyecto/README.md
@@ -1,5 +1,5 @@
 **Guessing Game**-
 Date and time of execution:
-jue may 21 11:51:49 -05 2020
+jue may 21 13:05:59 -05 2020
 -The number of lines of code in guessinggame:
-23
+30
--- Proyecto/guessinggame.sh
+++ Proyecto/guessinggame.sh
@@ -4,20 +4,27 @@ function files {
 	a=$(egrep "$" files.txt | wc -l)
 }
 files
-echo "Try to guess how many files does this directory has"
+echo "Try to guess how many files does this directory has (without counting the hiding files):"
 read element
 function guess {
 	while [[ $element -ne $a ]]
-	do
-		if [[ $element -gt $a ]]
+        do
+		if [[ $element =~ ^[1-9]+$ ]]  
 		then
-			echo "too high"
+			if [[ $element -gt $a ]]
+			then
+				echo "too high"
+				read element
+			elif [[ $element -lt $a ]]
+			then 
+				echo "too low"
+				read element
+			fi
+	        else 
+        	        echo "please enter a number (remember that the number of files are positive)"
 			read element
-		else 
-			echo "too low"
-			read element
-		fi
+       		fi
 	done
-	echo "Congrats"
+	echo "Congrats!"
 }
 guess
